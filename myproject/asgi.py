6bedf609429a9f0b808f776a4f5de4114b0d1e16
Sharjeel1906 <sharjeelahmed1906@gmail.com>
2026-06-01 15:12:28 +0500

    Fix ASGI initialization

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,10 +1,11 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import FYP_Partner_Finder.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
